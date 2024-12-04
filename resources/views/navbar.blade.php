661f558ea718f6b5c76dfd67888241d0c130df3d
Minuka hansaja <108428943+Minuka2022@users.noreply.github.com>
2024-12-04 21:53:13 +0530

    pulled changes

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,45 @@
+<header>
+    <div class="header-main sticky-nav ">
+        <div class="container position-relative">
+            <div class="row">
+                <div class="col-auto align-self-center">
+                    <div class="header-logo">
+                        <a href="index.html"><img src="assets/images/logo/logo.png" alt="Site Logo" /></a>
+                    </div>
+                </div>
+                <div class="col align-self-center d-none d-lg-block">
+                    <div class="main-menu">
+                        <ul>
+                            <li><a href="/">Home</a></li>
+
+                            <li><a href="/about">About us</a></li>
+                            <li><a href="/contact">Contact us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Header Action Start -->
+                <div class="col col-lg-auto align-self-center pl-0">
+                    <div class="header-actions">
+
+                        <!-- Single Wedge End -->
+                        <!-- Single Wedge Start -->
+                        <a href="#offcanvas-wishlist" class="header-action-btn offcanvas-toggle">
+                            <i class="pe-7s-like"></i>
+                        </a>
+                        <!-- Single Wedge End -->
+                        <a href="#offcanvas-cart"
+                           class="header-action-btn header-action-btn-cart offcanvas-toggle pr-0">
+                            <i class="pe-7s-shopbag"></i>
+                            <span class="header-action-num">01</span>
+                            <!-- <span class="cart-amount">€30.00</span> -->
+                        </a>
+                        <a href="#offcanvas-mobile-menu"
+                           class="header-action-btn header-action-btn-menu offcanvas-toggle d-lg-none">
+                            <i class="pe-7s-menu"></i>
+                        </a>
+                    </div>
+                    <!-- Header Action End -->
+                </div>
+            </div>
+        </div>
+</header>
